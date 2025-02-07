aef6de97487116798517d2ba53bfb43af35af828
David Elmo Ross <110311948+DavidElmoRoss@users.noreply.github.com>
2025-02-07 14:32:17 -0500

    Update main.cpp
    cleaned up comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
   Auther      : www.freenove.com
   Modification: 2024/06/19
 **********************************************************************/
-#include "arduino.h"
-#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
+#include "arduino.h"              // include arduino.h
+#include <LiquidCrystal_I2C.h>    // include LiquidCrysal_I2C.h
+#include <Wire.h>                 // include Wire.h
 
-#define SDA 13                    //Define SDA pins
-#define SCL 14                    //Define SCL pins
+#define SDA 13                    //Define SDA pin as pin 13
+#define SCL 14                    //Define SCL pin as pin 14
 
-LiquidCrystal_I2C lcd(0x27,16,2); 
+LiquidCrystal_I2C lcd(0x27,16,2); // lcd i2c address 0x27, 16 cols, 2 rows
 void setup() {
   Wire.begin(SDA, SCL);           // attach the IIC pin
 
@@ -24,14 +24,16 @@ void setup() {
 void loop() {
   lcd.setCursor(0,1);             // Move the cursor to row 1, column 0
   lcd.print("Counter:");          // The count is displayed every second
-  lcd.print(millis() / 1000);
+  lcd.print(millis() / 1000);     // print scaled millis on LCD
   
-  delay(1000);
+  delay(1000);                    // delay for 1 second
 }
 
-bool i2CAddrTest(uint8_t addr) {
+bool i2CAddrTest(uint8_t addr) 
+{
   Wire.beginTransmission(addr);
-  if (Wire.endTransmission() == 0) {
+  if (Wire.endTransmission() == 0) 
+  {
     return true;
   }
   return false;
